aa6d463c441f78cc2aa2005d4165af85fa028dee
Hendrick <hmusche@gmail.com>
2019-01-23 09:05:39 +0100

    enable touch events on gallery

--- template/js/Gallery.js
+++ template/js/Gallery.js
@@ -3,68 +3,122 @@ var Gallery = new Class({
         this.imageContainer = gallery.getElement('.image-wrapper');
         this.galleryWrapper = gallery.getElement('.gallery-wrapper');
         this.controls = gallery.getElement('.gallery-controls');
-
+        this.offset = 0;
         this.setImages();
     },
 
     setImages: function() {
         var self = this;
 
-        this.images = [];
-        this.currentIndex = 1;
+        this.images = this.galleryWrapper.getElements('img');
+        this.currentIndex = 0;
         this.loadedImages = 0;
-        this.minHeight = 100000;
 
-        this.imageContainer.getElements('img').each(function(img) {
-            var img = new Element('img', {
-                    'class': 'gallery-image',
-                    'src': img.get('src'),
-                    'data-index': self.currentIndex,
-                    'styles': {
-                        'max-width': window.innerWidth + 'px'
-                    },
-                    'events': {
-                        'load': function() {
-                            self.loadedImages++;
+        this.images.each(function(img) {
+            if (!img.get('src')) {
+                img.addEvent('load', function() {
+                    self.loadedImages++;
 
-                            if (this.clientHeight < self.minHeight) {
-                                self.minHeight = this.clientHeight;
-                            }
-
-                            if (self.loadedImages == self.images.length) {
-                                self.initGallery();
-                            }
-                        }
+                    if (self.loadedImages == self.images.length) {
+                        self.initGallery();
                     }
-                }),
-                wrapper = new Element('div', {
-                    'class': 'gallery-image-wrapper'
                 });
 
-            self.images.push(img);
-            img.inject(wrapper);
-            wrapper.inject(self.galleryWrapper);
+                img.set('src', img.get('data-src'));
+            }
         });
     },
 
-    initGallery: function() {
+    setSizes: function() {
         var self = this,
-            galleryWidth = 0;
+            galleryWidth = 0,
+            minHeight = 10000;
 
         this.images.each(function(image) {
-            image.setStyle('height', self.minHeight);
+            image.setStyle('height', 'auto');
+            image.setStyle('max-width', self.galleryWrapper.getParent().clientWidth + 'px');
+        });
+
+        this.images.each(function(image) {
+            if (image.clientHeight < minHeight) {
+                minHeight = image.clientHeight;
+            }
+        });
+
+        this.images.each(function(image) {
+            image.setStyle('height', minHeight + 'px');
+
             galleryWidth = galleryWidth + image.width;
         });
 
-        self.galleryWrapper.setStyle('width', galleryWidth + 'px');
+        self.galleryWrapper.setStyle('width', galleryWidth + 10 + 'px');
+        self.galleryWrapper.getParent().setStyle('height', minHeight + 'px');
 
-        this.showImage(0);
+        this.showImage();
+    },
+
+    initGallery: function() {
+        this.setSizes();
         this.initEvents();
         this.toggleEasing();
     },
 
     initEvents: function() {
-        var self = this;
+        var self = this,
+            current, initial, max, min;
+
+        this.galleryWrapper.getParent('.solsken-gallery').addEvents({
+            'touchstart': function(event) {
+                var elem = this;
+
+                self.toggleEasing();
+
+                current = max = min = 0;
+
+                initial = event.client.x;
+
+                this.store('was_touched', true);
+
+                timeout = setTimeout(function() {
+                    elem.eliminate('was_touched');
+                }, 400);
+            },
+            'touchmove': function(event) {
+                current = initial - event.client.x;
+
+                if (max < current) {
+                    max = current;
+                }
+
+                if (min > current) {
+                    min = current;
+                }
+
+                if (self.currentIndex == 0 && current < 0) {
+                    current = 0;
+                } else if (self.currentIndex == (self.images.length - 1) && current > 0) {
+                    current = 0;
+                }
+
+                self.galleryWrapper.setStyle('transform', 'translate(' + (-1 * (self.offset + current)) + 'px)');
+            },
+            'touchend': function(event) {
+                self.toggleEasing();
+
+                if (current >= max) {
+                    self.showImage(self.currentIndex + 1);
+                } else if (current <= min) {
+                    self.showImage(self.currentIndex - 1);
+                } else {
+                    self.showImage();
+                }
+            },
+            mouseover: function() {
+                if (!this.retrieve('was_touched')) {
+                    self.controls.addClass('has-pointer');
+                }
+            }
+        });
 
         this.controls.getElements('div').addEvent('click', function(e) {
             var direction = this.hasClass('gallery-left');
@@ -76,6 +130,10 @@ var Gallery = new Class({
             }
         });
 
+        window.addEvent('resize', function() {
+            self.setSizes();
+        })
+
         this.controls.addClass('done');
     },
 
@@ -84,6 +142,10 @@ var Gallery = new Class({
     },
 
     showImage: function(index) {
+        if (typeof index == 'undefined') {
+            index = this.currentIndex;
+        }
+
         if (!this.images[index]) {
             return;
         }
@@ -91,13 +153,24 @@ var Gallery = new Class({
         this.currentIndex = index;
 
         var wrapper = this.images[index].getParent('.gallery-image-wrapper'),
-            offset = wrapper.offsetLeft,
             width = this.images[index].clientWidth,
             viewportWidth = this.galleryWrapper.getParent('.solsken-gallery').clientWidth;
 
-        this.galleryWrapper.setStyle('margin-left', -1 * (offset - (viewportWidth - width) / 2) + 'px');
+        this.offset =  wrapper.offsetLeft;
+        this.galleryWrapper.setStyle('transform', 'translate(' + (-1 * this.offset) + 'px)');
 
         this.galleryWrapper.getElements('.gallery-image-wrapper').removeClass('active');
         wrapper.addClass('active');
+
+        this.controls.getElements('div').addClass('active');
+
+        if (this.currentIndex === 0) {
+            this.controls.getElements('div')[0].removeClass('active');
+        }
+
+
+        if (this.currentIndex == (this.images.length - 1)) {
+            this.controls.getElements('div')[1].removeClass('active');
+        }
     }
 });
