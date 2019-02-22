00f45be8e0995369ff19424ff4210d17b4044a4e
Hendrick <hmusche@gmail.com>
2019-02-22 19:35:06 +0100

    eplicitly set midnight for time diff

--- library/Util.php
+++ library/Util.php
@@ -9,6 +9,7 @@ class Util extends U {
     static public function daysDifference($start, $date) {
         $i18n  = I18n::getInstance();
         $start = \DateTime::createFromFormat('Y-m-d', $start);
+        $start->setTime(0, 0, 0);
         $date  = (new \DateTime())->setTimestamp($date);
 
         $diff = $start->diff($date);
