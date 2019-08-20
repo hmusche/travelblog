86f84d2dd2d50d44da5ca5889578a26cb12af5c6
Hendrick <hmusche@gmail.com>
2019-08-20 23:48:08 +0700

    RSS support for dc:creator and category

--- library/Controller/Feed.php
+++ library/Controller/Feed.php
@@ -26,25 +26,34 @@ class Feed extends Controller {
             'description' => $config['description']
         ]);
 
+        if (isset($config['category'])) {
+            $feed->setCategory($config['category']);
+        }
+
         foreach ($posts as $post) {
             $item = [
-                'title' => $post['title'],
-                'link' => $config['host'] . $config['path'] . $post['link'],
+                'title'       => $post['title'],
+                'link'        => $config['host'] . $config['path'] . $post['link'],
                 'description' => $post['text'],
-                'guid' => $config['host'] . $config['path'] . 'post/' . $post['id'],
-                'pubDate' => $post['posted']
+                'guid'        => $config['host'] . $config['path'] . 'post/' . $post['id'],
+                'pubDate'     => $post['posted'],
+                'dc:creator'  => $post['author']
             ];
 
             if ($post['files']) {
                 $files = $postMediaModel->getMedia($post['id'], 'sm');
 
+                if (file_exists("asset/{$post['id']}/sm/{$files[0]['filename']}")) {
+                    $item['enclosure'] = [
+                        'url' => $config['host'] . $config['path'] . $files[0]['full_path'],
+                        'type' => $files[0]['type'],
+                        'length' => filesize("asset/{$post['id']}/sm/{$files[0]['filename']}")
+                    ];
+                }
+            }
 
-                $item['enclosure'] = [
-                    'url' => $config['host'] . $config['path'] . $files[0]['full_path'],
-                    'type' => $files[0]['type'],
-                    'length' => filesize("asset/{$post['id']}/sm/{$files[0]['filename']}")
-                ];
-
+            if ($post['tag']) {
+                $item['category'] = explode(', ', $post['tag']);
             }
 
             $feed->addItem($item);
