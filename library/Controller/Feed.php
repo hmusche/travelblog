a0fa912d8e33ff89717f7fb9f36343ca970216b5
Hendrick <hmusche@gmail.com>
2019-08-20 17:25:00 +0700

    Add Feed Controller

---
+++ library/Controller/Feed.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace TravelBlog\Controller;
+
+use TravelBlog\Controller;
+use TravelBlog\Model\Post;
+
+use Solsken\Feed\Rss;
+use Solsken\Registry;
+
+class Feed extends Controller {
+    public function rssAction() {
+        header("Content-Type: application/xml; charset=utf-8");
+
+        $postModel = new Post;
+
+        $limit = 10;
+        $posts = $postModel->getPosts([], $limit);
+
+        $config = Registry::get('app.config');
+        $feed = new Rss([
+            'title' => $config['title'],
+            'link' => $config['host'] . $config['path'],
+            'description' => $config['description']
+        ]);
+
+        foreach ($posts as $post) {
+            $item = [
+                'title' => $post['title'],
+                'link' => $config['host'] . $config['path'] . $post['link'],
+                'description' => $post['text'],
+                'guid' => $config['host'] . $config['path'] . $post['id']
+            ];
+
+            $feed->addItem($item);
+        }
+
+        echo $feed->render();
+        exit;
+    }
+}
